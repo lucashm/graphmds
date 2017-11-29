a3b7bcf65bb45065ca7b4beaca904f9fea89dc5a
Lucas Malta <lucas50xd@hotmail.com>
2017-11-29 10:33:22 -0200

    adding buttons to save nodes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import Vis from '../node_modules/vis/dist/vis.js';
 import { asyncFetchUsers, mdsStudents, repositories, associations } from './fetchData.js';
 // import '../node_modules/vis/dist/vis.css';
 
+let pointOne = 0;
+let pointTwo = 0;
+let actualPoint = 0;
+
 const component = () => {
   let element = document.createElement('div');
   element.id = "maindiv";
@@ -21,7 +25,7 @@ const component = () => {
   let nodes = new Vis.DataSet(studentsObjects);
 
 
-  for (let association in associations){
+  for (let association in associations) {
     associationsObjects.push({ from: associations[association].user_one_id, to: (associations[association].user_two_id) })
   }
   console.log("ASSOCIAÇÕES AAAAAAAAAAAAAAAA")
@@ -36,15 +40,68 @@ const component = () => {
     edges: edges
   }
 
-  let options = { nodes: { color: 'red' }, layout : { improvedLayout: false } };
+  // let options = { , };
+
+  // these are all options in full.
+  let options = {
+    nodes: { color: 'red' }, layout: { improvedLayout: false },
+    physics: {
+      enabled: true,
+      barnesHut: {
+        gravitationalConstant: -2000,
+        centralGravity: 0.3,
+        springLength: 95,
+        springConstant: 0.04,
+        damping: 0.09,
+        avoidOverlap: 0
+      },
+      forceAtlas2Based: {
+        gravitationalConstant: -50,
+        centralGravity: 0.01,
+        springConstant: 0.08,
+        springLength: 100,
+        damping: 0.4,
+        avoidOverlap: 0
+      },
+      repulsion: {
+        centralGravity: 0.2,
+        springLength: 200,
+        springConstant: 0.05,
+        nodeDistance: 100,
+        damping: 0.09
+      },
+      hierarchicalRepulsion: {
+        centralGravity: 0.0,
+        springLength: 100,
+        springConstant: 0.01,
+        nodeDistance: 120,
+        damping: 0.09
+      },
+      maxVelocity: 50,
+      minVelocity: 0.1,
+      solver: 'forceAtlas2Based',
+      stabilization: {
+        enabled: true,
+        iterations: 1000,
+        updateInterval: 100,
+        onlyDynamicEdges: false,
+        fit: true
+      },
+      timestep: 0.5,
+      adaptiveTimestep: true
+    }
+  }
 
 
   const network = new Vis.Network(container, data, options);
 
+  network.setOptions(options);
+
   network.on("click", function (params) {
     params.event = "[original event]";
-    document.getElementById('eventSpan').innerHTML = '<h2>Click event:</h2>' + JSON.stringify(params, null, 4)
+    document.getElementById('eventSpan').innerHTML = "Node: " + this.getNodeAt(params.pointer.DOM);
     console.log('click event, getNodeAt returns: ' + this.getNodeAt(params.pointer.DOM));
+    actualPoint = this.getNodeAt(params.pointer.DOM);
   });
 
 
@@ -54,7 +111,31 @@ const component = () => {
 const callback = () => {
   let mainComponent = component();
   document.body.appendChild(mainComponent);
+  let buttonOne = document.createElement("button");
+  buttonOne.innerHTML = "Assign to \"from\"";
+  buttonOne.onclick = () => onClickEvent(1);
+
+  let buttonTwo = document.createElement("button");
+  buttonTwo.innerHTML = "Assign to \"to\"";
+  buttonTwo.onclick = () => onClickEvent(2);
+
+  let selectComponent = document.createElement("div");
+  selectComponent.id = "selectDiv";
+  selectComponent.innerHTML = '<p> from: ' + pointOne + ' to: ' + pointTwo
+  document.body.appendChild(selectComponent);
+  document.body.appendChild(buttonOne);
+  document.body.appendChild(buttonTwo);
 }
 
 document.onload = asyncFetchUsers(callback);
 
+
+const onClickEvent = (selector) => {
+  let selectComponent = document.getElementById("selectDiv");
+  if (selector == 1) {
+    pointOne = actualPoint;
+  } else {
+    pointTwo = actualPoint;
+  }
+  selectComponent.innerHTML = '<p> from: ' + pointOne + ' to: ' + pointTwo
+}
